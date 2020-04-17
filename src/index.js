f82bd6c3eaaaa7a623d1f162f2ceb8f175812c20
seanaleid <seanaleid@gmail.com>
2020-04-16 23:36:18 -0400

    Remove onUpdate from index.js, not needed

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import ScrollToTop from "./ScrollToTop.js";
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
-  <Router onUpdate={() => window.scrollTo(0,0)}>
+  <Router >
     <ScrollToTop />
     <App />
   </Router>, document.getElementById('root'));
